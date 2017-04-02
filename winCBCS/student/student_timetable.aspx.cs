190a649beea274b29ebc152d7b815b17895514dd
rkdevmore <rajanikantdevmore@gmail.com>
2017-04-02 13:46:19 +0530

    student time instudent

---
+++ winCBCS/student/student_timetable.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using winCBCS.utility;
+
+namespace winCBCS.student
+{
+    public partial class WebForm7 : System.Web.UI.Page
+    {
+        string faculty_id;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadList();
+        }
+
+        private void LoadList()
+        {
+            try
+            {
+                timetable.DataSource = DBConnection.GetDataTable("SELECT * FROM faculty_choice natural join timetable_subject where (faculty_id='" + faculty_id + "')");
+                timetable.DataBind();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
